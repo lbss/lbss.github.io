711e8bde094417136b64052a828e39f94fb54d3b
ch987 <chris.ch.987@gmail.com>
2015-05-18 13:27:57 -0400

    js
    most js some html

--- LBSS.js
+++ LBSS.js
@@ -10,11 +10,37 @@ function display() { //shows map
     document.getElementById("data").innerHTML = obj;
 }
 
+function sendInput() { //send the input data to sheet
+    'use strict';
+    var instreet, incity, instate, inzip;
+    event.preventDefault();
+    instreet = document.getElementById("istreet").value;
+    incity = document.getElementById("icity").value;
+    instate = document.getElementById("istate").value;
+    inzip = document.getElementById("izip").value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", 'https://script.google.com/macros/s/1rctbH3LEpumJ8ZOEjWz1F-K4-BAGm3hY9L9CM8I4rTw/exec?create&name=' + instreet + '&pass=' + inzip, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                //Data successfully submitted!
+                alert("Input Successful!");
+            } else {
+                //Error
+                //alert("Error! Registration Unsuccessful");
+                alert("Error");
+            }
+        }
+    };
+    xhr.send();
+}
+
 $(document).ready(function () { //READY
     'use strict';
     
     window.onload = function () { //onclick functions
         document.getElementById("displayMap").onclick = display;
+        document.getElementById("sendIt").onclick = sendInput;
     };
     
 });
